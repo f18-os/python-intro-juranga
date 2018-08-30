f1b45b0b695b262c450b8ad1ac451801d71b6088
jerry <urangagerardo@gmail.com>
2018-08-29 19:41:36 -0600

    added comment, removed unnecessary code

--- wordCount.py
+++ wordCount.py
@@ -11,8 +11,6 @@ wordDict = defaultdict(int)
 # attempt to open input file
 with open(inputFile, 'r') as file:
     for line in file:
-        # get rid of newline characters
-        line = line.strip()
         # split line on whitespace and punctuation
         wordList = re.split('[ \t]|[\W+]', line)
         for word in wordList:
@@ -21,8 +19,8 @@ with open(inputFile, 'r') as file:
             word = word.lower()
             wordDict[word] = wordDict[word] + 1
 
+# Sort the dictionary by key values
 wordDict = OrderedDict(sorted(wordDict.items()))
-
 with open(outputFile, 'w+') as output:
     for word in wordDict:
         output.write(word + ' ' + str(wordDict[word]) + '\n')
