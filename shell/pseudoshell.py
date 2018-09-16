e649f8daf9b145ac37167c89a7fd377f5a9d945a
Gerardo Uranga <urangagerardo@gmail.com>
2018-09-16 13:32:19 -0600

    finished lab with pipe redirection working

--- shell/pseudoshell.py
+++ shell/pseudoshell.py
@@ -22,7 +22,7 @@ def shell_logic(shell):
     if shell < 0:
         os.write(2, ("fork failed, returning %d\n"% rc).encode())
         sys.exit(1)
-    elif shell == 0:
+    elif shell == 0: # child
         args = []
         os.write(sys.stdout.fileno(), display_path.encode())
         command = input().split()
@@ -33,31 +33,29 @@ def shell_logic(shell):
         while idx < command_len-1:
             idx = idx + 1
             if command[idx] == '|' and idx+1 < command_len:
-                idx = idx + 1
                 pipefds = os.pipe()
-                args = []
+                #args = []
                 child = os.fork()
                 if child == 0:
                     os.close(pipefds[0])
                     os.dup2(pipefds[1], os.sys.stdout.fileno())
                     os.close(pipefds[1])
-                    execute(command[0:idx-1])
-                    #sys.exit(0)
+                    break
                 else:
-                    args.append(command[idx])
-                    #args.append(os.fdopen(pipefds[0]).read()[0:-1])
-                    os.close(os.sys.stdin.fileno())
+                    os.wait()
+                    args = []
+                    os.close(pipefds[1])
                     os.dup2(pipefds[0], os.sys.stdin.fileno())
                     os.close(pipefds[0])
-                continue
-            elif command[idx] == '<' and idx+1 < command_len:
+                idx = idx + 1
+            if command[idx] == '<' and idx+1 < command_len:
                 idx = idx + 1
                 os.close(sys.stdin.fileno())
                 sys.stdin = open(command[idx])
                 fd = sys.stdin.fileno()
                 os.set_inheritable(fd, True)
                 continue
-            elif command[idx] == '>' and idx+1 < command_len:
+            if command[idx] == '>' and idx+1 < command_len:
                 idx = idx + 1
                 os.close(sys.stdout.fileno())
                 sys.stdout = open(command[idx], "w")
